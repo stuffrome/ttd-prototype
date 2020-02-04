9c04dd6840ab198c8acdce706ba708b6cb22c050
Daniel Bramwell <dbramwell@ufl.edu>
2020-02-04 18:52:10 -0500

    added assets and missing scripts

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        UIManager.Instance.IncreaseScore(ScorePoints);
+        Destroy(this.gameObject);
+    }
+
+    public int ScorePoints = 100;
+    public float rotateSpeed = 50f;
+}
