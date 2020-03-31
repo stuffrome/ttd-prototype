b8ea88c894f2d3fff8591e3b1e501169e6d39abe
Daniel Bramwell <dbramwell@ufl.edu>
2020-03-31 17:40:25 -0400

    Imported UI and 2 non-working thunderclouds. UI works perfectly by itself

---
+++ Assets/Scripts/ClickToLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToLoad : MonoBehaviour
+{
+	public void LoadTutorial()
+	{
+		SceneManager.LoadScene("TTD_Tutorial");
+    }
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("TTD_StartScreen");
+	}
+}
---
+++ Assets/Scripts/TTD_PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TTD_PauseMenu : MonoBehaviour
+{
+	public static bool gameIsPaused = false;
+	public GameObject pauseMenu;
+	AudioSource audioSource;
+
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+		{
+            if (gameIsPaused)
+			{
+				resumeGame();
+			} else
+			{
+				pauseGame();
+			}
+		}
+    }
+
+	public void resumeGame()
+	{
+		pauseMenu.SetActive(false);
+		Time.timeScale = 1f;
+		gameIsPaused = false;
+	}
+
+	public void pauseGame()
+	{
+		pauseMenu.SetActive(true);
+		Time.timeScale = 0f;
+		gameIsPaused = true;
+	}
+
+	public void toMenu()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("TTD_StartScreen");
+	}
+
+	public void quitGame()
+	{
+		Application.Quit();
+	}
+
+    public void muteAudio()
+	{
+		audioSource.mute = !audioSource.mute;
+	}
+}
